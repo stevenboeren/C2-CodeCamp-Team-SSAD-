1db9ccf15667153ac48dffb48bbfb93d8b8d97a9
Steven Boeren <70892306+stevenboeren@users.noreply.github.com>
2021-10-08 15:13:12 +0200

    Header beginnetje

---
+++ header.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="container header">
+        <h1>MediaPortal</h1>
+        <nav>
+            <a href="<?php echo $base_url; ?>/index.php">Home</a>
+        </nav>
+    </div>
+</header>
