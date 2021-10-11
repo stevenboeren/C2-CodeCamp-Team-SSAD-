3517a9a85641688c8e5bf39eb83d9ef3d88a5203
Steven Boeren <D281107@edu.rocwb.nl>
2021-10-11 12:15:41 +0200

    #2 Header gemaakt

---
+++ head.php
@@ -0,0 +1,23 @@
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
--- header.php
+++ header.php
@@ -2,7 +2,8 @@
     <div class="container header">
         <h1>MediaPortal</h1>
         <nav>
-            <a href="<?php echo $base_url; ?>/index.php">Home</a>
+            <a href="<?php echo $base_url; ?>/index.php">Home</a> |
+            <a href="<?php echo $base_url; ?>/contact.php">Contact</a>
         </nav>
     </div>
 </header>
