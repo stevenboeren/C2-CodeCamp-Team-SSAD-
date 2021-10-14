297c8752b14fad16a51f037ff5921fe0fc451892
Steven Boeren <D281107@edu.rocwb.nl>
2021-10-14 11:37:55 +0200

    Head aangepast + Base Url toegevoegd

---
+++ backend/config.php
@@ -0,0 +1,3 @@
+<?php
+//De url waarop jouw project staat. Géén slash aan het einde.
+$base_url = 'http://localhost/C2-CodeCamp-Team-SSADJ-';
--- head.php
+++ head.php
@@ -1,6 +1,6 @@
 <head>
   <meta charset="utf-8">
-  <title></title>
+  <title>MediaPortal</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -14,8 +14,11 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- Place favicon.ico in the root directory -->
 
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/main.css">
+  <?php require_once 'backend/config.php'; ?>
+  <link rel="icon" href="<?php echo $base_url; ?>/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="<?php echo $base_url; ?>/css/normalize.css">
+  <link rel="stylesheet" href="<?php echo $base_url; ?>/css/main.css">
+  <link rel="stylesheet" href="<?php echo $base_url; ?>/css/colors.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
