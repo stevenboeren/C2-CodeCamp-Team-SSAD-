2bc93005774ce17e536f9677faef7a1db0d62ab9
SaskiaZwarts <71122362+SaskiaZwarts@users.noreply.github.com>
2021-10-15 09:47:28 +0200

    Songteksten html/css
    Selectie pagina aangemaakt voor songteksten. Aparte pagina voor elk liedje gemaakt. En deze pagina's mooi gemaakt met css. :-)

---
+++ songteksten/index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<?php require_once '../head.php' ?>
+
+<body>
+
+  <?php require_once '../header.php' ?>
+
+  <main>
+    <div class="container selection">
+
+        <div class="welcome-message">
+            <h2>Gereedschappen</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit at sit odio aliquam est, molestiae tempora, esse nulla necessitatibus quisquam animi repellendus libero, unde iure ullam debitis possimus sapiente porro.</p>
+        </div>
+        
+        <div class="grid">
+            <div class="grid-item" style="background-image: url('<?php echo $base_url; ?>/img/songteksten/bohemianrhapsody.png')">
+                <div class="item-text"><a href="<?php echo $base_url; ?>/songteksten/liedjes/bohemianrhapsody.php">Bohemian Rhapsody - Queen</a></div>
+            </div>
+            <div class="grid-item" style="background-image: url('<?php echo $base_url; ?>/img/songteksten/killerqueen.jpg')">
+                <div class="item-text"><a href="<?php echo $base_url; ?>/songteksten/liedjes/killerqueen.php">Killer Queen - Queen</a></div>
+            </div>
+            <div class="grid-item" style="background-image: url('<?php echo $base_url; ?>/img/songteksten/wearethechampions.jpg')">
+                <div class="item-text"><a href="<?php echo $base_url; ?>/songteksten/liedjes/wearethechampions.php">We are the champions - Queen</a></div>
+            </div>
+            <div class="grid-item" style="background-image: url('<?php echo $base_url; ?>/img/songteksten/madness.jpg')">
+                <div class="item-text"><a href="<?php echo $base_url; ?>/songteksten/liedjes/madness.php">Madness - Muse</a></div>
+            </div>
+            <div class="grid-item" style="background-image: url('<?php echo $base_url; ?>/img/songteksten/uprisingmuse.jpg')">
+                <div class="item-text"><a href="<?php echo $base_url; ?>/songteksten/liedjes/uprisingmuse.php">Uprising - Muse</a></div>
+            </div>
+            <div class="grid-item" style="background-image: url('<?php echo $base_url; ?>/img/songteksten/pluginbaby.jpg')">
+                <div class="item-text"><a href="<?php echo $base_url; ?>/songteksten/liedjes/pluginbaby.php">Plug in baby - Muse</a></div>
+            </div>
+            <div class="grid-item" style="background-image: url('<?php echo $base_url; ?>/img/songteksten/uprisingmybaby.jpg')">
+                <div class="item-text"><a href="<?php echo $base_url; ?>/songteksten/liedjes/uprisingmybaby.php">Uprising - MyBaby</a></div>
+            </div>
+            <div class="grid-item" style="background-image: url('<?php echo $base_url; ?>/img/songteksten/transylvania.jpg')">
+                <div class="item-text"><a href="<?php echo $base_url; ?>/songteksten/liedjes/transylvania.php">Transylvia - Iron Maiden</a></div>
+            </div>
+        </div>
+
+    </div>
+
+  </main>
+
+  <?php require_once '../footer.php' ?>
+
+</body>
+
+</html>
---
+++ songteksten/liedjes/bohemianrhapsody.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<?php require_once '../../head.php' ?>
+
+<body>
+
+  <?php require_once '../../header.php' ?>
+
+  <main>
+    <div class="container item-page item-page-songtekst">
+        <h3>Bohemian Rhapsody - Queen</h3>
+        <div class="page-content-format">
+            <div class="left">
+                <h4>Bohemian Rhapsody - Queen</h4>
+                <div class="item-information-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis maximus consequat. Sed rhoncus ultrices leo, vel commodo urna aliquam quis. Cras ornare eleifend neque non vestibulum. Ut eget arcu orci. Morbi vitae odio a dui lacinia faucibus. Curabitur ullamcorper odio nec ex ullamcorper, ut lacinia nisl iaculis. In sollicitudin efficitur tempor. Nulla facilisi. Curabitur dapibus tempus blandit. In mattis tincidunt quam, eget blandit lorem dignissim eu. In vel luctus lacus, ut gravida neque.</p>
+                    <p>Mauris eget mollis odio. Cras blandit mi sit amet est lobortis mattis. Curabitur fermentum vestibulum vestibulum. In dignissim libero vitae leo euismod scelerisque. Aenean fringilla, turpis lobortis sodales scelerisque, sapien lectus vehicula lacus, quis dictum dolor lacus nec dui. Integer quis tempor velit, sed aliquam enim. Fusce lectus nulla, consectetur et maximus eget, dapibus ut magna. Vestibulum ut tincidunt nibh, lacinia ornare nibh. Nunc placerat pellentesque porta. Pellentesque finibus ex et risus fringilla, in condimentum odio lacinia. Ut eu tempor tellus, et feugiat nisl. Fusce accumsan efficitur diam, nec imperdiet odio varius at. Suspendisse posuere auctor libero eget dictum.</p>
+                    <p>Sed dignissim finibus velit eget laoreet. Donec euismod malesuada ligula vel tincidunt. Curabitur porta condimentum ex sed pretium. Proin accumsan massa sed tincidunt maximus. Sed et mi a mauris commodo volutpat quis sit amet est. Curabitur sodales mollis placerat. Cras sollicitudin tortor a auctor consequat. Quisque pretium enim vel sem ultrices, id congue felis fermentum. Nam leo leo, venenatis in elit sed, accumsan rhoncus libero. Aenean cursus tincidunt enim, eu sagittis nibh sodales a. Fusce a sem erat.</p>
+                    <p>Sed porttitor massa eget orci gravida scelerisque a sit amet tellus. Proin vitae tellus ac dolor ultricies lacinia a sed diam. Nam at ultrices erat. Etiam a dolor quis ipsum accumsan ullamcorper. Duis nec felis a purus ultricies egestas a nec leo. Suspendisse potenti. Donec consequat ultricies urna in aliquam. Quisque egestas, justo ac eleifend gravida, elit quam lobortis orci, vel porttitor elit diam quis nulla. Cras facilisis dignissim volutpat. Nunc ante leo, eleifend placerat consequat ac, mattis non quam.</p>
+                </div>
+            </div>
+            <div class="right">
+                <img src="<?php echo $base_url; ?>/img/songteksten/bohemianrhapsody.png" alt="Bohemian Rhapsody">
+                <div class="song-info">
+                    <p><span class="bold-text">Release date:</span> 31 oktober 1975</p>
+                    <p><span class="bold-text">Genre:</span> Progressive rock - hard rock - progressive pop</p>
+                    <p><span class="bold-text">Lengte:</span> 5:55</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  </main>
+
+  <?php require_once '../../footer.php' ?>
+
+</body>
+
+</html>
