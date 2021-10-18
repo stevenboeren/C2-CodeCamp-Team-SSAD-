2c99cb257a593b1194e226511c94c3324349c89d
Steven Boeren <D281107@edu.rocwb.nl>
2021-10-18 12:12:06 +0200

    Frisdranken pagina's in Dranken aangemaakt

--- dranken/frisdranken/frisdranken.php
+++ dranken/frisdranken/frisdranken.php
@@ -3,7 +3,7 @@
 
 <?php require_once '../../head.php' ?>
 
-<body class="purple">
+<body class="orange">
 
   <?php require_once '../../header.php' ?>
 
@@ -13,34 +13,34 @@
       <div class="page-content-format">
             <div class="left">
                 <div class="item-information-text">
-                  <h3>Thee</h3>
-                  <p>Thee is een warme drank die wordt gemaakt door infusie van de gedroogde bladeren van de theeplant Camellia sinensis. <br> Alle als thee verkochte producten die niet van deze plant afkomstig zijn (bijvoorbeeld kamillethee of rooibos), zijn dan ook geen echte thee maar kruidenthee.</p>     
+                  <h3>Cola</h3>
+                  <p>Cola is een koolzuurhoudende frisdrank. Het smaakt zoet (maar is qua pH zuur) en bevat circa 28-32 mg cafeïne per 33 cl. Andere belangrijke ingrediënten zijn vanille, kaneel, karamel en citroen. <br> Anders dan de meeste frisdranken bevat het geen citroenzuur, maar fosforzuur.</p>     
                 </div>
             </div>
             <div class="right">
-                <img id="drink-img" src="<?php echo $base_url; ?>/img/dranken/warmedranken/thee.jpg" alt="Thee">
+                <img id="drink-img" src="<?php echo $base_url; ?>/img/dranken/frisdranken/cola.jpg" alt="Cola">
             </div>
         </div>
         <div class="page-content-format">
             <div class="left">
                 <div class="item-information-text">
-                  <h3>Koffie</h3>
-                  <p>Koffie is een meestal warm genuttigde drank, die wordt bereid op basis van water en gedroogde en gebrande pitten van de koffieplant (Coffea spp.) die vanwege hun vorm koffiebonen worden genoemd. <br> Koffie bevat het stimulerende middel cafeïne.</p>    
+                  <h3>7Up</h3>
+                  <p>7up of Sevenup is een bruisende drank (prik door koolzuur) die van smaak lijkt op Sprite. 7up wordt verkocht in een groene, plastic fles. Het heeft een lemon-lime (citroen-limoen) smaak en is doorzichtig. </p>    
                 </div>
             </div>
             <div class="right">
-                <img id="drink-img" src="<?php echo $base_url; ?>/img/dranken/warmedranken/koffie.jpeg" alt="koffie">
+                <img id="drink-img" src="<?php echo $base_url; ?>/img/dranken/frisdranken/sevenup.jpg" alt="sevenup">
             </div>
         </div>
         <div class="page-content-format">
             <div class="left">
                 <div class="item-information-text">
-                  <h3>Latte macchiato</h3>
-                  <p>Een latte macchiato is een koffiedrank die bestaat uit laagjes van melk en espresso. Latte betekent melk en macchiato staat voor gevlekt. <br> Wat er ontstaat als er op de juiste wijze espresso aan de latte wordt toegevoegd is een vlek in de melk. </p>   
+                  <h3>Sinas</h3>
+                  <p>Sinas is in Nederland de benaming voor de doorgaans gele of oranje, meestal koolzuurhoudende frisdrank met sinaasappelsmaak. Zoals de meeste frisdranken bevat sinas veel suiker of in de light-versie kunstmatige zoetstoffen. </p>   
                 </div>
             </div>
             <div class="right">
-                <img id="drink-img" src="<?php echo $base_url; ?>/img/dranken/warmedranken/latte.jpg" alt="latte">
+                <img id="drink-img" src="<?php echo $base_url; ?>/img/dranken/frisdranken/sinas.jpg" alt="sinas">
             </div>
         </div>
       
