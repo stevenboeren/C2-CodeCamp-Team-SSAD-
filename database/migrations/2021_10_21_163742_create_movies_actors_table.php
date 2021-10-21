3ca24a830a6f2cfc55530557c9bc0ae494151915
Steven Boeren <D281107@edu.rocwb.nl>
2021-10-21 19:01:39 +0200

    Database Kolommen Toegevoegd met Models

--- database/migrations/2021_10_21_163742_create_movies_actors_table.php
+++ database/migrations/2021_10_21_163742_create_movies_actors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateMoviesActorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('movies_actors', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('description');
+            $table->string('img-url');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('movies_actors');
     }
 }
