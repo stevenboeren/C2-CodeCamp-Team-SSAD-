ad7a4ee04e1c61bb0fc064b23ef11e30119fb10e
Steven Boeren <D281107@edu.rocwb.nl>
2021-11-08 13:07:37 +0100

    Tabellen toegevoegd in admin pagina

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -14,4 +14,7 @@ Route::group([
     ),
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+    Route::crud('books', 'BooksCrudController');
+    Route::crud('users', 'UsersCrudController');
+    Route::crud('lyrics', 'LyricsCrudController');
 }); // this should be the absolute last line of this file
